e9e99a957fadae2f104fdf5db6207ebcd0f95d30
agussoul <aalma@mail.austral.edu.ar>
2026-03-20 15:45:45 -0300

    added logo

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // Treat `src/public/` as Vite's static public directory so files there are
+  // available at the site root (e.g. for favicon).
+  publicDir: 'src/public',
   plugins: [react()],
 })
